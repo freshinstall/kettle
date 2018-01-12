52394ba4bef33787e48ad1fd351fc3dbcbae30f4
Ian Santopietro <ian@system76.com>
2018-01-12 15:52:29 -0700

    Begin working on actual functionality

---
+++ kettle/action.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+## kettle - Desktop software configuration manager
+# Copyright (c) 2018, freshinstall
+# All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED “AS IS” AND ISC DISCLAIMS ALL WARRANTIES WITH REGARD 
+# TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS. IN NO EVENT SHALL ISC BE LIABLE FOR  ANY SPECIAL, DIRECT, INDIRECT,
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+# DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+# TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+# OF THIS SOFTWARE.
+#
+# action.py - actions to take
+
+import logging, os
+import gettext
+_ = gettext.gettext
+
+# generic action class
+class Action():
+
+    def __init__(self):
+        # Set up some basic logging
+        self.log = logging.getLogger('kettle.Action')
+        self.log.debug(_("Logging set up!"))
+        print("done")
+
+    def create(self, path):
+        self.log.debug(_("Starting creation of new kettle:  %s" % path))
+
+        # do some stuff
+        print("done")
+
+        self.log.debug(_("%s created!" % path))
+
+    def install(self, path):
+        self.log.debug(_("Starting installation of %s kettle" % path))
+
+        # do some stuff
+
+        self.log.debug(_("Installed %s" % path))
