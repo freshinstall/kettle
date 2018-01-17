2fc9c6dc95f2a4e81a8d57834a85b3669886ed49
Ian Santopietro <ian@system76.com>
2018-01-17 11:52:44 -0700

    Add comments explaining what things do.

--- kettle/ket.py
+++ kettle/ket.py
@@ -18,40 +18,69 @@
 #
 # ket.py - Kettle object class
 
+# Here we import some helpful modules to do more stuff with python
 import tarfile
 import yaml
 import glob, os
 import logging
 
+# And we set up the translations again.
 import gettext
 _ = gettext.gettext
 
+# This is for some advanced Error handling.
 class BadKettle(Exception):
     pass
 
+# This class is the python representation of a kettle itself.
 class Kettle():
-    name = ""
-    ketid = ""
-    modules = []
-    permissions = {}
-    standard = 0
-    tmppath = "/tmp/kettle/"
-    kettle_yaml = []
 
+    # First we have the attributes. These describe the kettle.
+    # All of these are set using the code below this block
+    name = ""                 # The name of the kettle, from the yaml file
+    ketid = ""                # The id field of the kettle's yaml
+    modules = []              # What plugins the kettle will load.
+    permissions = {}          # What permissions the kettle requests.
+    standard = 0              # The standards version of the kettle
+    tmppath = "/tmp/kettle/"  # This is the path in /tmp we'll use if we need to
+    kettle_yaml = []          # This is a representation of every field in the
+                              # kettle YAML.
+
+    # Next we set up the class's methods. Methods are actions that classes can
+    # perform. They work exactly like functions.
     def __init__(self, path):
-        self.path = path
+        # __init__ is the method that is called when we create an object. Any
+        # arguments we give to the object will be passed to this method.
+        # Additionally, it will set up all of the attributes and get a lot of
+        # information. Here we pass the path to the kettle file as the path
+        # argument.
 
+        self.path = path # This creates a new attribute of our kettle called "path"
+                         # It stores the path of the kettle file. We set it to
+                         # The value of the "path" variable we got as
+                         # an argument.
+
+        # This is where we open up the Kettle file for reading. Since Kettles are
+        # tar archives, we use the tarfile module. Again, we wrap this in a
+        # try/except block because we don't yet know if the user gave us a path
+        # to a kettle that exists, and need to present an error if it doesn't.
         try:
+            # "kettle_ark" is the representation of the kettle archive
             self.kettle_ark = tarfile.open(name=self.path, mode='r')
         except FileNotFoundError:
+            # Here we're looking if the above block failed with a
+            # "FileNotFoundError" exception, and we're sending out a
+            # "BadKettle" exception instead.
             raise BadKettle(_('The kettle doesn\'t exist'))
 
-        self.get_yaml()
-        self.get_name()
-        self.get_id()
-        self.get_modules()
-        self.get_permissions()
+        # These methods all populate the class attributes with actual information
+        self.get_yaml()         # This gets the yaml
+        self.get_name()         # This gets the name
+        self.get_id()           # This gets the id
+        self.get_modules()      # This gets a list of modules the kettle needs
+        self.get_permissions()  # This gets the list of permissions the kettle requests.
 
+        # Here we set the tmppath attribute to /tmp/kettle/the_id_of_the_kettle
         self.tmppath = "/tmp/kettle/%s" % self.ketid
 
     def get_yaml(self):
